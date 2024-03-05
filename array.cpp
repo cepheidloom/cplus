0ae4c85fa23e6046b770c95192025effd79a9ea6
cepheidloom <manishpratapsingh@jklu.edu.in>
2024-03-05 21:20:00 +0530

    Edited classes.cpp

--- array.cpp
+++ array.cpp
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include <iostream>
 
 int main() {
--- classes.cpp
+++ classes.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <string>
 
-using std::string; 
-enum Mood { HAPPY, CURIOUS, SEARCH };
+using namespace std;
 
-class lol {
+enum Brand { ANDROID, IOS, KEY_PAD };
+
+class Phone {
 public:
-  lol(){};
-  lol(const std::string &state, bool sma);
-  void setMood();
-  int getAge() { return 23; }
+  Phone();
+  Phone(const string &nm, const int &prc, const string &osystem,
+        const Brand &brnd);
+  int getPrice() { return 29; }
+  string getName() { return name; }
+  Brand getBrand() { return brand; }
 
 private:
-  long pain;
-  short sight;
-  bool misrey;
-  std::string progress;
+  string name;
+  long price;
+  string os;
+  Brand brand;
 };
 
+// main starts
 int main() {
-  lol twenty_four;
-  std::cout << twenty_four.getAge() << std::endl;
+  Phone p1;
+  Phone p2("Iphone", 25000, "apple OS", IOS);
+  Phone p3 = p2;
+  cout << p1.getName() << endl;
+  cout << p2.getName() << endl;
+  cout << p3.getName() << endl;
+}
+// main ends
+
+Phone::Phone() {
+  name = "Nokia";
+  price = 25000;
+  os = "custom";
+  brand = ANDROID;
+}
+Phone::Phone(const string &nm, const int &prc, const string &osystem,
+             const Brand &brnd) {
+  name = nm;
+  price = prc;
+  os = osystem;
+  brand = brnd;
 }
